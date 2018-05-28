68552bb340df7c5a48b1c5b78be7e77c8305cb53
investmentblockchain <investmentblockchain@gmail.com>
2018-05-28 13:29:55 +0300

    new version

--- src/version.h
+++ src/version.h
@@ -10,16 +10,16 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 80010; //
+static const int PROTOCOL_VERSION = 80600; //
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70900;
+static const int GETHEADERS_VERSION = 80500;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70900; //
+static const int MIN_PEER_PROTO_VERSION = 80500; //
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
